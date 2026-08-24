79ab6c5726809815c25908fae67cb33dfcb2f04f
googoochadwick <arush.anand.singh.3121@gmail.com>
2026-08-24 23:13:55 +0530

    lesson 3

--- main.sql
+++ main.sql
@@ -1,2 +1,7 @@
--- SELECT '...' || ... ;
-select 'Hello, ' || 'SQL!';
+CREATE TABLE cities (name TEXT, country TEXT);
+INSERT INTO cities VALUES ('Tokyo', 'Japan');
+INSERT INTO cities VALUES ('Paris', 'France');
+INSERT INTO cities VALUES ('Cairo', 'Egypt');
+
+-- write your SELECT here
+select cities from cities;
